2e4965507881511d7628210e0169c65fd12850f2
Punith03-vv <121851100+Punith03-vv@users.noreply.github.com>
2023-09-21 15:50:50 +0530

    first commit

---
+++ src/app/About/page.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <>
+    <div className="About-border">
+      <h1 className='center'>About</h1>
+      <h2 className='content'>
+      Real estate development is a process that involves the purchase of raw land, rezoning, construction and renovation of buildings, and sale or lease of the finished product to end users. Developers earn a profit by adding value to the land creating buildings or improvements, rezoning, etc.
+      </h2>
+      <h2 className='team'>--Team SUIET</h2>
+    </div>
+    </>
+  )
+}
+
+export default page
---
+++ src/app/Forgot/page.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import Link from 'next/link'
+
+const page = () => {
+  return (
+    <>
+    <div className="full-screen">
+    <div className="border">
+      <h2>Forgot Password</h2>
+      <input className='email' type='text' placeholder='Email Address'></input>
+      <input className='email' type='password' placeholder='Password'></input>
+      <input className='email' type='password' placeholder='Confirm password'></input>
+      <Link href=''><button className='sub'>Submit</button></Link>
+    </div>
+    </div>
+    </>
+  )
+}
+
+export default page
